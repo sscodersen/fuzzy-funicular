758670af5bc421c0d7d87462cdd8ad47e3047798
sscodersen <marketing.businessblueprint@gmail.com>
2023-05-04 20:17:27 -0700

    fix

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,5 @@
 import { SearchQuery, Source } from "@/types";
-import { IconArrowRight, IconSearch } from "@tabler/icons-react";
+import { IconArrowRight, IconSearch, IconBolt } from "@tabler/icons-react";
 import endent from "endent";
 import { FC, KeyboardEvent, useEffect, useRef, useState } from "react";
 
@@ -139,7 +139,7 @@ export const Search: FC<SearchProps> = ({ onSearch, onAnswerUpdate, onDone }) =>
       ) : (
         <div className="mx-auto flex h-full w-full max-w-[750px] flex-col items-center space-y-6 px-3 pt-32 sm:pt-64">
           <div className="flex items-center">
-            <IconHoneycomb size={36} />
+            <IconBolt size={36} />
             <div className="ml-1 text-center text-4xl">Nectar</div>
           </div>
 
