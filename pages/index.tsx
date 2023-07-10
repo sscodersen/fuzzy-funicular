54f95d085c7abd856aef04a94cface960811dc0a
socialtribexyz <msclubnft@gmail.com>
2023-07-10 16:53:29 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         </a>
 
         <a
-          className="absolute top-0 left-2 p-4 cursor-pointer"
+          className="absolute top-0 center-2 p-4 cursor-pointer"
           href="/promptbar"
           target="_blank"
           rel="noreferrer"
