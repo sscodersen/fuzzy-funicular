d1abaa908fb549fe5935cdbcc9b21785640797ea
socialtribexyz <msclubnft@gmail.com>
2023-07-10 16:36:26 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           target="_blank"
           rel="noreferrer"
         >
-          <IconBrandTwitter />
+          <IconBell />
         </a>
 
         <a
