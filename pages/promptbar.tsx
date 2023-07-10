f8bc31d0e9601e914732bb6009a026dac61bdd70
socialtribexyz <msclubnft@gmail.com>
2023-07-10 15:23:43 -0700

    update

--- pages/promptbar.tsx
+++ pages/promptbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function promptbar() {
   return (
     <div>
-      <iframe src="https://nextjs-chatgpt-app-enricoros.vercel.app" width="100%" height="700px" title="Embedded Content" />
+      <iframe src="https://nextjs-chatgpt-app-enricoros.vercel.app" width="100%" height="695px" title="Embedded Content" />
     </div>
   );
 }
