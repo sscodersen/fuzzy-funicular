5a1ffa95ed5ea07a4741355596a3190461b65c00
socialtribexyz <msclubnft@gmail.com>
2023-07-10 12:50:08 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Clarity AI</title>
+        <title>NectarGPT</title>
         <meta
           name="description"
           content="AI-powered search."
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="h-screen overflow-auto bg-[#18181C] text-[#D4D4D8]">
         <a
           className="absolute top-0 right-12 p-4 cursor-pointer"
-          href="https://twitter.com/mckaywrigley"
+          href="https://www.threads.net/@officialsphrex"
           target="_blank"
           rel="noreferrer"
         >
@@ -39,7 +39,7 @@ export default function Home() {
 
         <a
           className="absolute top-0 right-2 p-4 cursor-pointer"
-          href="https://github.com/mckaywrigley/clarity-ai"
+          href="https://github.com/socialtribexyz/Nectar-GPT"
           target="_blank"
           rel="noreferrer"
         >
