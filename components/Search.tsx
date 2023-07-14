6d3e26315b8eeb20a1a6777182ad23fcea17ec63
socialtribexyz <msclubnft@gmail.com>
2023-07-13 22:19:12 -0700

    update

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,6 +1,7 @@
 import { IconArrowRight, IconSearch, IconWand } from "@tabler/icons-react";
 import { FC, KeyboardEvent, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { PromptBar } from "/PromptBar";
 
 interface SearchProps {
   onSearch: () => void;
@@ -37,7 +38,7 @@ export const Search: FC<SearchProps> = ({ onSearch }) => {
         />
 
         <button>
-          <Link to="/promptbar.tsx">
+          <Link to="/PromptBar">
             <IconArrowRight className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-blue-500 p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10" />
           </Link>
         </button>
