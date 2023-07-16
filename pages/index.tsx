7200e401cb1d24bea0631dff746e5b6292158793
socialtribexyz <msclubnft@gmail.com>
2023-07-15 21:22:39 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,30 +10,51 @@ export default function Home() {
   const [answer, setAnswer] = useState<string>("");
   const [done, setDone] = useState<boolean>(false);
 
-  const handleSearch = (result: SearchQuery) => {
-    setSearchQuery(result);
-  };
-
   return (
     <>
       <Head>
         <title>NectarGPT</title>
-        <meta name="description" content="AI-powered search." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="AI-powered search."
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link
+          rel="icon"
+          href="/favicon.png"
+        />
       </Head>
       <div className="h-screen overflow-auto bg-[#18181C] text-[#D4D4D8]">
-        <a className="absolute top-0 right-12 p-4 cursor-pointer" href="https://www.threads.net/@officialsphrex" target="_blank" rel="noreferrer">
+        <a
+          className="absolute top-0 right-12 p-4 cursor-pointer"
+          href="https://www.threads.net/@officialsphrex"
+          target="_blank"
+          rel="noreferrer"
+        >
           <IconBrandTwitter />
         </a>
 
-        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="" target="_blank" rel="noreferrer">
+        <a
+          className="absolute top-0 right-2 p-4 cursor-pointer"
+          href="/promptbar"
+          target="_blank"
+          rel="noreferrer"
+        >
           <IconNotification />
         </a>
 
-        <a className="absolute top-0 left-2 p-4 cursor-pointer" href="" target="_blank" rel="noreferrer">
+        <a
+          className="absolute top-0 left-2 p-4 cursor-pointer"
+          href=""
+          target="_blank"
+          rel="noreferrer"
+        >
           <IconGlobe size={24} />
         </a>
+
 
         {answer ? (
           <Answer
@@ -48,7 +69,7 @@ export default function Home() {
           />
         ) : (
           <Search
-            onSearch={handleSearch}
+            onSearch={setSearchQuery}
             onAnswerUpdate={(value) => setAnswer((prev) => prev + value)}
             onDone={setDone}
           />
