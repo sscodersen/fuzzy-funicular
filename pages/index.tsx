c2d3ac5618dcbf28313a77f7bdcee676414b8b6c
socialtribexyz <msclubnft@gmail.com>
2023-10-01 15:28:25 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <IconAt />
         </a>
 
-        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://drive.google.com/uc?export=download&id=1-kayYVUn03pI4KpU39kOkwNgVbtDdHgN" target="_blank" rel="noreferrer">
+        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://resources.nectarhub.xyz/" target="_blank" rel="noreferrer">
           <IconNotification />
         </a>
 
