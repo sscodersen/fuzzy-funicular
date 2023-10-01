8be994234192ebb0ae1e253b47953c2bb49becd8
socialtribexyz <msclubnft@gmail.com>
2023-10-01 15:24:01 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { IconBrandat, IconNotification, IconGlobe, IconWand, IconSearch, IconArrowRight } from "@tabler/icons-react";
+import { Iconat, IconNotification, IconGlobe, IconWand, IconSearch, IconArrowRight } from "@tabler/icons-react";
 import Head from "next/head";
 import { useState } from "react";
 
@@ -20,7 +20,7 @@ export default function Home() {
           target="_blank"
           rel="noreferrer"
         >
-          <IconBrandat />
+          <Iconat />
         </a>
 
         <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://drive.google.com/uc?export=download&id=1-kayYVUn03pI4KpU39kOkwNgVbtDdHgN" target="_blank" rel="noreferrer">
