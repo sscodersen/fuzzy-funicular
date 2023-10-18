24be526380e9f0af77c390bb1a35731e2c535377
socialtribexyz <msclubnft@gmail.com>
2023-10-18 13:17:28 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,50 +13,50 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="h-screen overflow-auto bg-[#18181C] text-[#D4D4D8]">
-        <a
-          className="absolute top-0 right-12 p-4 cursor-pointer"
-          href="https://www.threads.net/@officialsphrex"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconAt />
-        </a>
+      <div className="h-screen bg-[#18181C] text-[#D4D4D8] flex flex-row">
+        {/* Sidebar */}
+        <div className="w-16 bg-[#1A1A1D] p-4 flex flex-col justify-between">
+          <a href="https://www.threads.net/@officialsphrex" target="_blank" rel="noreferrer">
+            <IconAt />
+          </a>
 
-        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://resources.nectarhub.xyz/" target="_blank" rel="noreferrer">
-          <IconNotification />
-        </a>
+          <a href="https://resources.nectarhub.xyz" target="_blank" rel="noreferrer">
+            <IconNotification />
+          </a>
 
-        <a className="absolute top-0 left-2 p-4 cursor-pointer" href="https://prompt.nectarhub.xyz/" target="_blank" rel="noreferrer">
-          <IconGlobe size={24} />
-        </a>
+          <a href="https://prompt.nectarhub.xyz" target="_blank" rel="noreferrer">
+            <IconGlobe size={24} />
+          </a>
+        </div>
 
-        {showSearch && (
-          <div className="mx-auto flex h-full w-full max-w-[750px] flex-col items-center space-y-6 px-3 pt-32 sm:pt-64">
-            <div className="flex items-center">
-              <IconWand />
-              <div className="ml-1 text-center text-4xl">NectarGPT</div>
+        {/* Main Content */}
+        <div className="flex-grow p-4">
+          {showSearch && (
+            <div className="max-w-xl mx-auto space-y-6">
+              <div className="flex items-center">
+                <IconWand className="text-[#D4D4D8] text-4xl" />
+                <div className="ml-2 text-[#D4D4D8] text-4xl">NectarGPT</div>
+              </div>
+
+              <div className="relative">
+                <input
+                  className="w-full h-12 bg-transparent border-b-2 border-[#D4D4D8] text-[#D4D4D8] placeholder-[#D4D4D8] focus:outline-none focus:border-blue-500"
+                  type="text"
+                  placeholder="What would you like to achieve today..."
+                />
+
+                <a href="https://ai.nectarhub.xyz">
+                  <IconArrowRight className="absolute right-0 top-0 mt-1 mr-2 h-7 w-7 text-blue-500 cursor-pointer hover:text-blue-600" />
+                </a>
+              </div>
             </div>
+          )}
 
-            <div className="relative w-full">
-              <IconSearch className="text=[#D4D4D8] absolute top-3 w-10 left-1 h-6 rounded-full opacity-50 sm:left-3 sm:top-4 sm:h-8" />
-
-              <input
-                className="h-12 w-full rounded-full border border-zinc-600 bg-[#2A2A31] pr-12 pl-11 focus:border-zinc-800 focus:bg-[#18181C] focus:outline-none focus:ring-2 focus:ring-zinc-800 sm:h-16 sm:py-2 sm:pr-16 sm:pl-16 sm:text-lg"
-                type="text"
-                placeholder="What would you like today..."
-              />
-
-              <a href="https://ai.nectarhub.xyz/">
-                <button>
-                  <IconArrowRight
-                    className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-blue-500 p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10"
-                  />
-                </button>
-              </a>
-            </div>
+          {/* Add your GIF or visual element here */}
+          <div className="mt-8 text-center">
+            <img src="your-gif-url.gif" alt="Talking AI" />
           </div>
-        )}
+        </div>
       </div>
     </>
   );
