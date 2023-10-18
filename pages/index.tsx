55022eaec5940b52d7bca4a5b53149f5817e6ee2
socialtribexyz <msclubnft@gmail.com>
2023-10-18 13:31:48 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { IconAt, IconNotification, IconGlobe, IconWand, IconSearch, IconArrowRight } from "@tabler/icons-react";
 import Head from "next/head";
 import { useState } from "react";
+import './your-styles.css';
 
 export default function Home() {
-  const [showSearch, setShowSearch] = useState<boolean>(true);
+  const [showSearch, setShowSearch] = useState(true);
 
   return (
     <>
@@ -14,20 +15,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <div className="h-screen overflow-auto bg-[#18181C] text-[#D4D4D8]">
-        <a
-          className="absolute top-0 right-12 p-4 cursor-pointer"
-          href="https://www.threads.net/@officialsphrex"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a className="absolute top-0 right-12 p-4 cursor-pointer" href="https://www.threads.net/@officialsphrex" target="_blank" rel="noreferrer">
           <IconAt />
         </a>
 
-        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://resources.nectarhub.xyz/" target="_blank" rel="noreferrer">
+        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://resources.nectarhub.xyz" target="_blank" rel="noreferrer">
           <IconNotification />
         </a>
 
-        <a className="absolute top-0 left-2 p-4 cursor-pointer" href="https://prompt.nectarhub.xyz/" target="_blank" rel="noreferrer">
+        <a className="absolute top-0 left-2 p-4 cursor-pointer" href="https://prompt.nectarhub.xyz" target="_blank" rel="noreferrer">
           <IconGlobe size={24} />
         </a>
 
@@ -39,19 +35,18 @@ export default function Home() {
             </div>
 
             <div className="relative w-full">
-              <IconSearch className="text=[#D4D4D8] absolute top-3 w-10 left-1 h-6 rounded-full opacity-50 sm:left-3 sm:top-4 sm:h-8" />
+              <IconSearch className="text-[#D4D4D8] absolute top-3 w-10 left-1 h-6 rounded-full opacity-50 sm:left-3 sm:top-4 sm:h-8" />
 
               <input
-                className="h-12 w-full rounded-full border border-zinc-600 bg-[#2A2A31] pr-12 pl-11 focus:border-zinc-800 focus:bg-[#18181C] focus:outline-none focus:ring-2 focus:ring-zinc-800 sm:h-16 sm:py-2 sm:pr-16 sm:pl-16 sm:text-lg"
+                className="h-12 w-full rounded-full border border-zinc-600 bg-[#2A2A31] pr-12 pl-11 focus:border-zinc-800 focus:bg-[#18181C] focus:outline-none focus:ring-2 focus:ring-zinc-800 sm:h-16 sm-py-2 sm-pr-16 sm-pl-16 sm-text-lg"
                 type="text"
                 placeholder="What would you like today..."
+                defaultValue="Search for something..."
               />
 
-              <a href="https://ai.nectarhub.xyz/">
+              <a href="https://ai.nectarhub.xyz" target="_blank" rel="noreferrer">
                 <button>
-                  <IconArrowRight
-                    className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-blue-500 p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10"
-                  />
+                  <IconArrowRight className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-blue-500 p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10" />
                 </button>
               </a>
             </div>
@@ -61,3 +56,4 @@ export default function Home() {
     </>
   );
 }
+
